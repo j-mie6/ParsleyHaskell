886e4f8520880287c6da64f96fe4bf71e9070123
Jamie Willis <j.willis19@imperial.ac.uk>
2020-01-23 14:32:27 +0000

    Completed the first version of the queue, I'll improve this later

--- Queue.hs
+++ Queue.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module Queue (Queue, empty, enqueue, dequeue, null) where
 import Prelude hiding (null)
 
@@ -6,17 +7,24 @@ data Queue a = Queue {
   outs  :: [a],
   insz  :: Int,
   ins   :: [a]
-}
+} deriving Eq
 
 empty :: Queue a
 empty = Queue 0 [] 0 []
 
 enqueue :: a -> Queue a -> Queue a
-enqueue = undefined
+enqueue x q = q {insz = insz q + 1, ins = x : ins q}
 
 dequeue :: Queue a -> (a, Queue a)
-dequeue = undefined
+dequeue q@(outs -> (x:outs')) = (x, q {outsz = outsz q - 1, outs = outs'})
+dequeue q@(outs -> []) = dequeue (Queue (insz q) (reverse (ins q)) 0 [])
 
 null :: Queue a -> Bool
 null (Queue 0 [] 0 []) = True
 null _ = False
+
+size :: Queue a -> Int
+size q = insz q + outsz q
+
+instance Show a => Show (Queue a) where
+  show q = show (outs q ++ reverse (ins q))

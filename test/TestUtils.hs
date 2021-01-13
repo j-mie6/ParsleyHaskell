72df629d445e1f0b3030bd78f32b0f9faad29b9a
Jamie Willis <j.willis19@imperial.ac.uk>
2021-01-13 10:26:57 +0000

    Added deepseq, which makes testing coverage more accurate

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, GeneralizedNewtypeDeriving, TypeApplications #-}
+{-# LANGUAGE TemplateHaskell, TypeApplications, DeriveAnyClass, StandaloneDeriving #-}
 module TestUtils where
 
 import Parsley (runParser, Parser, Code)
@@ -6,13 +6,59 @@ import Parsley (runParser, Parser, Code)
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.TestUtils
 import Language.Haskell.TH.TestUtils.QMode
+import Control.DeepSeq
 
 -- TODO Use WQ: requires lift plugin to not require any Lift instance for variables (if missing)
 runParserMocked :: Parser a -> Code (Parser a) -> Code (String -> Maybe a)
 runParserMocked p qp = [|| \s ->
-    runParserMocked' $$qp `seq` $$(runParser p) s
+    runParserMocked' $$qp `deepseq` $$(runParser p) s
   ||]
 
--- Use DeepSeq instead, the either thing is a mess frankly
-runParserMocked' :: Parser a -> ()
-runParserMocked' = either error (const ()) . tryTestQ (QState MockQ [] []) . unTypeQ . runParser @String
+runParserMocked' :: Parser a -> Exp
+runParserMocked' = runTestQ (QState MockQ [] []) . unTypeQ . runParser @String
+
+deriving instance NFData Exp
+deriving instance NFData Name
+deriving instance NFData OccName
+deriving instance NFData NameFlavour
+deriving instance NFData ModName
+deriving instance NFData NameSpace
+deriving instance NFData PkgName
+deriving instance NFData Lit
+deriving instance NFData Type
+deriving instance NFData TyVarBndr
+deriving instance NFData Pat
+deriving instance NFData TyLit
+deriving instance NFData Match
+deriving instance NFData Body
+deriving instance NFData Guard
+deriving instance NFData Stmt
+deriving instance NFData Dec
+deriving instance NFData Clause
+deriving instance NFData Con
+deriving instance NFData Bang
+deriving instance NFData SourceUnpackedness
+deriving instance NFData SourceStrictness
+deriving instance NFData DerivClause
+deriving instance NFData Range
+deriving instance NFData DerivStrategy
+deriving instance NFData FunDep
+deriving instance NFData Overlap
+deriving instance NFData Foreign
+deriving instance NFData Callconv
+deriving instance NFData Fixity
+deriving instance NFData FixityDirection
+deriving instance NFData Safety
+deriving instance NFData Pragma
+deriving instance NFData Inline
+deriving instance NFData TySynEqn
+deriving instance NFData RuleMatch
+deriving instance NFData TypeFamilyHead
+deriving instance NFData FamilyResultSig
+deriving instance NFData Phases
+deriving instance NFData Role
+deriving instance NFData InjectivityAnn
+deriving instance NFData RuleBndr
+deriving instance NFData PatSynArgs
+deriving instance NFData AnnTarget
+deriving instance NFData PatSynDir

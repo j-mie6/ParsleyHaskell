81cf7a58a4d0d080246627d3bcdf414e6e3988ab
Jamie Willis <j.willis19@imperial.ac.uk>
2021-08-23 14:26:19 +0100

    Switched to text

--- parsley/benchmarks/BrainfuckBench/Parsley/Parser.hs
+++ parsley/benchmarks/BrainfuckBench/Parsley/Parser.hs
@@ -12,6 +12,9 @@ import Parsley.Fold (skipMany)
 --import Parsley.Garnish
 import Language.Haskell.TH.Syntax (Lift(..))
 
+import Parsley.Register
+import Parsley.Defunctionalized
+
 deriving instance Lift BrainFuckOp
 
 brainfuck :: Parser [BrainFuckOp]
@@ -34,3 +37,30 @@ brainfuck = whitespace *> bf <* eof
     op '.' = item $> [|Output|]
     op ',' = item $> [|Input|]
     op '[' = between (lexeme item) (try (char ']')) ([|Loop|] <$> bf)
+
+-- This is as closed to the handrolled version as it's possible to get: it's /very/ fast
+-- If register elimination can be performed, this would be equivalent to the handrolled I think
+brainfuck' :: Parser [BrainFuckOp]
+brainfuck' = newRegister_ EMPTY $ \acc ->
+  let walk :: Parser [BrainFuckOp]
+      -- This `eof` is interesting
+      -- The "obvious" way of thinking about this is to just move that `gets_` clause last
+      -- This works because `item` only fails if `eof` wouldn't have done.
+      -- However, at the /moment/, Parsley knows that `eof`'s failure doesn't consume input, and
+      -- optimises the handlers appropriately, but the scope of the failure of the match covers
+      -- the cases too, and so failing there generates a length check etc. Interestingly, the fix
+      -- here is to add a `try` (!!!), which improves performance considerably (but GHC then decides
+      -- not to inline something to make them otherwise identical). That's wild.
+      walk = eof *> gets_ acc [|reverse|]
+         <|> lookAhead (char ']') *> gets_ acc [|reverse|]
+         <|> {- try ( -}match "><+-.,[" item op walk -- )
+         -- <|> gets_ acc [|reverse|]
+      op :: Char -> Parser [BrainFuckOp]
+      op '>' = modify_ acc (APP_H CONS (LIFTED RightPointer)) *> walk
+      op '<' = modify_ acc (APP_H CONS (LIFTED LeftPointer)) *> walk
+      op '+' = modify_ acc (APP_H CONS (LIFTED Increment)) *> walk
+      op '-' = modify_ acc (APP_H CONS (LIFTED Decrement)) *> walk
+      op '.' = modify_ acc (APP_H CONS (LIFTED Output)) *> walk
+      op ',' = modify_ acc (APP_H CONS (LIFTED Input)) *> walk
+      op '[' = modify acc (CONS <$> ([|Loop|] <$> local acc (pure EMPTY) (walk <* char ']'))) *> walk
+  in walk <* eof

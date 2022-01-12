5c314c2c106c9ccda5b536b7a8404ea3ac17c381
Jamie Willis <j.willis19@imperial.ac.uk>
2022-01-12 19:03:28 +0000

    Added a block to the Put instruction, fixing #41

--- parsley/test/Regression/Parsers.hs
+++ parsley/test/Regression/Parsers.hs
@@ -5,6 +5,8 @@ import Prelude hiding (pure, (<*>), (*>), (<*))
 import Data.Char (isDigit)
 import Parsley
 import Parsley.Char (token)
+import Parsley.Register (newRegister_, put_, get)
+import Parsley.Defunctionalized (Defunc(LIFTED))
 --import Parsley.Garnish
 
 issue26_ex1 :: Parser ()
@@ -12,3 +14,6 @@ issue26_ex1 = (token "123" <|> token "") *> void (token "abc")
 
 issue26_ex2 :: Parser ()
 issue26_ex2 = (token "123" <|> token "45") *> void (token "abc")
+
+issue41_ex1 :: Parser Bool
+issue41_ex1 = newRegister_ (LIFTED False) $ \reg -> optional (try (char 'a' *> put_ reg (LIFTED True) *> char 'b')) *> get reg
--- parsley/test/Regression/Tests.hs
+++ parsley/test/Regression/Tests.hs
@@ -12,7 +12,7 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Regression Tests" [ issue26 ]
+tests = testGroup "Regression Tests" [ issue26, issue41 ]
 
 issue26 :: TestTree
 issue26 = testGroup "#26 Coin draining on bindings is wrong"
@@ -20,8 +20,18 @@ issue26 = testGroup "#26 Coin draining on bindings is wrong"
   , testCase "" $ issue26_ex2 (CharList "123ab") @?= Nothing
   ]
 
+issue41 :: TestTree
+issue41 = testGroup "#41 Length-factoring can cross `put` but shouldn't"
+  [ testCase "it should work when enough input is given" $ issue41_ex1 (CharList "ab") @?= Just True
+  , testCase "it should work when enough input is given" $ issue41_ex1 (CharList "ac") @?= Just True
+  , testCase "it should prevent factoring" $ issue41_ex1 (CharList "a") @?= Just True
+  ]
+
 issue26_ex1 :: CharList -> Maybe ()
 issue26_ex1 = $$(Parsley.parse Parsers.issue26_ex1)
 
 issue26_ex2 :: CharList -> Maybe ()
 issue26_ex2 = $$(Parsley.parse Parsers.issue26_ex2)
+
+issue41_ex1 :: CharList -> Maybe Bool
+issue41_ex1 = $$(Parsley.parse Parsers.issue41_ex1)

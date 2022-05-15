d34e244193a42082d015110fc6b938cc7b3e0556
Jamie Willis <j.willis19@imperial.ac.uk>
2022-05-15 18:00:47 +0100

    Rolled out some of the balancing

--- parsley-core/src/ghc/Parsley/Internal/Common/RangeSet.hs
+++ parsley-core/src/ghc/Parsley/Internal/Common/RangeSet.hs
@@ -342,53 +342,79 @@ maxDelete !sz !l !u !lt !rt = let (# !ml, !mu, !_, t' #) = go sz l u lt rt in (#
 {-# INLINABLE balance #-}
 balance :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
 balance !sz !l !u lt rt
-  | height lt > height rt + 1 = balanceL sz l u lt rt
-  | height rt > height lt + 1 = balanceR sz l u lt rt
+  | height lt > height rt + 1 = uncheckedBalanceL sz l u lsz ll lu llt lrt rt
+  | height rt > height lt + 1 = uncheckedBalanceR sz l u lt rsz rl ru rlt rrt
   | otherwise = forkSz sz l u lt rt
+  where
+    Fork _ lsz ll lu llt lrt = lt
+    Fork _ rsz rl ru rlt rrt = rt
 
-{-# NOINLINE balanceL #-}
+{-# INLINEABLE balanceL #-}
 balanceL :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
 -- PRE: left grew or right shrank, difference in height at most 2 biasing to the left
-balanceL !sz !l1 !u1 lt@(Fork !hlt !szl !l2 !u2 !llt !rlt) !rt
+balanceL !sz !l1 !u1 lt@(Fork !lh !lsz !l2 !u2 !llt !lrt) !rt
   -- both sides are equal height or off by one
   | dltrt <= 1 = forkSz sz l1 u1 lt rt
   -- The bias is 2 (dltrt == 2)
-  | hllt >= hrlt = rotr sz l1 u1 lt rt
-  | otherwise    = rotr sz l1 u1 (rotl szl l2 u2 llt rlt) rt
+  | otherwise  = uncheckedBalanceL sz l1 u1 lsz l2 u2 llt lrt rt
   where
-    !dltrt = hlt - height rt
-    !hllt = height llt
-    !hrlt = height rlt
+    !dltrt = lh - height rt
 -- If the right shrank (or nothing changed), we have to be prepared to handle the Tip case for lt
 balanceL sz l u Tip rt | height rt <= 1 = forkSz sz l u Tip rt
 balanceL _ _ _ Tip _ = error "Right should have shrank, but is still 1 taller than a Tip!"
 
-{-# NOINLINE balanceR #-}
+{-# INLINEABLE balanceR #-}
 balanceR :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
 -- PRE: left shrank or right grew, difference in height at most 2 biasing to the right
-balanceR !sz !l1 !u1 !lt rt@(Fork !hrt szr l2 u2 lrt rrt)
+balanceR !sz !l1 !u1 !lt rt@(Fork !rh rsz l2 u2 rlt rrt)
   -- both sides are equal height or off by one
-  | drtlt <= 1 = forkSz sz l1 u1 lt rt
-  -- The bias is 2 (drtlt == 2)
-  | hrrt >= hlrt = rotl sz l1 u1 lt rt
-  | otherwise    = rotl sz l1 u1 lt (rotr szr l2 u2 lrt rrt)
+  | dltrt <= 1 = forkSz sz l1 u1 lt rt
+  | otherwise  = uncheckedBalanceR sz l1 u1 lt rsz l2 u2 rlt rrt
   where
-    !drtlt = hrt - height lt
-    !hlrt = height lrt
-    !hrrt = height rrt
+    !dltrt = rh - height lt
 -- If the left shrank (or nothing changed), we have to be prepared to handle the Tip case for rt
 balanceR sz l u lt Tip | height lt <= 1 = forkSz sz l u lt Tip
 balanceR _ _ _ _ Tip = error "Left should have shrank, but is still 1 taller than a Tip!"
 
+{-# NOINLINE uncheckedBalanceL #-}
+-- PRE: left grew or right shrank, difference in height at most 2 biasing to the left
+uncheckedBalanceL :: Size -> E -> E -> Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a -> RangeSet a
+uncheckedBalanceL !sz !l1 !u1 !szl !l2 !u2 !llt !lrt !rt
+  -- The bias is 2 (dltrt == 2)
+  | hllt >= hlrt = rotr' sz l1 u1 szl l2 u2 llt lrt rt
+  | otherwise    = rotr sz l1 u1 (rotl szl l2 u2 llt lrt) rt
+  where
+    !hllt = height llt
+    !hlrt = height lrt
+
+{-# NOINLINE uncheckedBalanceR #-}
+uncheckedBalanceR :: Size -> E -> E -> RangeSet a -> Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
+-- PRE: left shrank or right grew, difference in height at most 2 biasing to the right
+uncheckedBalanceR !sz !l1 !u1 !lt szr l2 u2 rlt rrt
+  -- The bias is 2 (drtlt == 2)
+  | hrrt >= hrlt = rotl' sz l1 u1 lt szr l2 u2 rlt rrt
+  | otherwise    = rotl sz l1 u1 lt (rotr szr l2 u2 rlt rrt)
+  where
+    !hrlt = height rlt
+    !hrrt = height rrt
+
 {-# INLINE rotr #-}
 rotr :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
-rotr !sz !l1 !u1 (Fork _ szl l2 u2 p q) !r = forkSz sz l2 u2 p (forkSz (sz - szl + size q) l1 u1 q r)
+rotr !sz !l1 !u1 (Fork _ szl l2 u2 p q) !r = rotr' sz l1 u1 szl l2 u2 p q r
 rotr _ _ _ _ _ = error "rotr on Tip"
 
 {-# INLINE rotl #-}
 rotl :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
-rotl !sz !l1 !u1 !p (Fork _ szr l2 u2 q r) = forkSz sz l2 u2 (forkSz (sz - szr + size q) l1 u1 p q) r
+rotl !sz !l1 !u1 !p (Fork _ szr l2 u2 q r) = rotl' sz l1 u1 p szr l2 u2 q r
 rotl _ _ _ _ _ = error "rotr on Tip"
+
+{-# INLINE rotr' #-}
+rotr' :: Size -> E -> E -> Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a -> RangeSet a
+rotr' !sz !l1 !u1 !szl !l2 !u2 !p !q !r = forkSz sz l2 u2 p (forkSz (sz - szl + size q) l1 u1 q r)
+
+{-# INLINE rotl' #-}
+rotl' :: Size -> E -> E -> RangeSet a -> Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
+rotl' !sz !l1 !u1 !p !szr !l2 !u2 !q !r = forkSz sz l2 u2 (forkSz (sz - szr + size q) l1 u1 p q) r
 
 {-|
 Unions two sets together such that if and only if an element appears in either one of the sets, it

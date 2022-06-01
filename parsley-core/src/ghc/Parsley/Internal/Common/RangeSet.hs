d8a47f9a84e411af47c46345b688260bd34dd0c7
Jamie Willis <j.willis19@imperial.ac.uk>
2022-06-01 11:37:19 +0100

    Added missing bang patterns

--- parsley-core/src/ghc/Parsley/Internal/Common/RangeSet.hs
+++ parsley-core/src/ghc/Parsley/Internal/Common/RangeSet.hs
@@ -351,7 +351,7 @@ balanceL sz l u Tip rt = forkSz sz l u Tip rt
 {-# INLINEABLE balanceR #-}
 balanceR :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
 -- PRE: left shrank or right grew, difference in height at most 2 biasing to the right
-balanceR !sz !l1 !u1 !lt rt@(Fork !rh rsz l2 u2 rlt rrt)
+balanceR !sz !l1 !u1 !lt rt@(Fork !rh !rsz !l2 !u2 !rlt !rrt)
   -- both sides are equal height or off by one
   | dltrt <= 1 = forkSz sz l1 u1 lt rt
   | otherwise  = uncheckedBalanceR sz l1 u1 lt rsz l2 u2 rlt rrt
@@ -374,7 +374,7 @@ uncheckedBalanceL !sz !l1 !u1 !szl !l2 !u2 !llt !lrt !rt
 {-# NOINLINE uncheckedBalanceR #-}
 uncheckedBalanceR :: Size -> E -> E -> RangeSet a -> Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
 -- PRE: left shrank or right grew, difference in height at most 2 biasing to the right
-uncheckedBalanceR !sz !l1 !u1 !lt szr l2 u2 rlt rrt
+uncheckedBalanceR !sz !l1 !u1 !lt !szr !l2 !u2 !rlt !rrt
   -- The bias is 2 (drtlt == 2)
   | hrrt >= hrlt = rotl' sz l1 u1 lt szr l2 u2 rlt rrt
   | otherwise    = rotl sz l1 u1 lt (rotr szr l2 u2 rlt rrt)
@@ -384,12 +384,12 @@ uncheckedBalanceR !sz !l1 !u1 !lt szr l2 u2 rlt rrt
 
 {-# INLINE rotr #-}
 rotr :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
-rotr !sz !l1 !u1 (Fork _ szl l2 u2 p q) !r = rotr' sz l1 u1 szl l2 u2 p q r
+rotr !sz !l1 !u1 (Fork _ !szl !l2 !u2 !p !q) !r = rotr' sz l1 u1 szl l2 u2 p q r
 rotr _ _ _ _ _ = error "rotr on Tip"
 
 {-# INLINE rotl #-}
 rotl :: Size -> E -> E -> RangeSet a -> RangeSet a -> RangeSet a
-rotl !sz !l1 !u1 !p (Fork _ szr l2 u2 q r) = rotl' sz l1 u1 p szr l2 u2 q r
+rotl !sz !l1 !u1 !p (Fork _ !szr !l2 !u2 !q !r) = rotl' sz l1 u1 p szr l2 u2 q r
 rotl _ _ _ _ _ = error "rotr on Tip"
 
 {-# INLINE rotr' #-}

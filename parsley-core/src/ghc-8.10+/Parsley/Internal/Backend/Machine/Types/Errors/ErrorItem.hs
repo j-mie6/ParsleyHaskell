c76a3693411288bbf17864a81eb407e1c0102f97
Jamie Willis <j.willis19@imperial.ac.uk>
2022-08-09 20:21:26 +0100

    Changed the Ord instance for ErrorItem, but it's not a legal Ord

--- parsley-core/src/ghc-8.10+/Parsley/Internal/Backend/Machine/Types/Errors/ErrorItem.hs
+++ parsley-core/src/ghc-8.10+/Parsley/Internal/Backend/Machine/Types/Errors/ErrorItem.hs
@@ -8,4 +8,12 @@ data ErrorItem where
   Raw :: String -> ErrorItem
   Desc :: String -> ErrorItem
   EndOfInput :: ErrorItem
-  deriving stock (Lift, Eq, Show, Ord)
+  deriving stock (Lift, Eq, Show)
+
+-- This is not a legal instance!
+instance Ord ErrorItem where
+  _ <= EndOfInput   = True
+  EndOfInput <= _   = False
+  _ <= Desc{}       = True
+  Desc{} <= _       = False
+  Raw cs <= Raw cs' = length cs <= length cs'
